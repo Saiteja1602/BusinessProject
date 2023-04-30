12427e3146d8e7c580c24b814bea79c2f92c50b6
Sai Teja <saitejaponnuri007@gmail.com>
2023-04-30 13:11:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+var ans1  = document.getElementById(example1);
+     var ans2 = document.getElementById(example2);
+ 
+     console.log(ans1);
+
+
+// var form = document.getElementById("form").addEventListener(('submit'),(event)=>{
+//      event.preventDefault();
+// })
+// var ans = document.getElementById(liveAlertBtn).addEventListener(('click'), ()=>{
+     
+//     answer();
+// })
+
+
+
+
+// document.createAttribute('class');
+// document.getElementById(teja).innerHTML("Thank youfor submitting the Form")
+
+// const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+// const appendAlert = (message, type) => {
+//   const wrapper = document.createElement('div')
+//   wrapper.innerHTML = [
+//     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+//     `   <div>${message}</div>`,
+//     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+//     '</div>'
+//   ].join('')
+
+//   alertPlaceholder.append(wrapper)
+// }
+
+// const alertTrigger = document.getElementById('liveAlertBtn')
+// if (alertTrigger) {
+//   alertTrigger.addEventListener('click', () => {
+//     appendAlert("Thank you for submitting the Form")
+//   })
+// }
